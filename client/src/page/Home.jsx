0fcbf2af14d8d95b713e2022896827f24d4e36e2
Cynthia Omovoiye <omovoiye@gmail.com>
2023-07-23 13:53:19 +0100

    Update

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -24,14 +24,12 @@ const Home = () => {
 
   const fetchPosts = async () => {
     setLoading(true);
-
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://dalle-ai-h1di.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': '*'
-
         },
       });
 
